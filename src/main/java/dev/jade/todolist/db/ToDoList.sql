5d10513fe96b79a5e1f9dffe5874dbc207d53bf3
Nyan Min Htet <nyanminhtet2304@gmail.com>
2026-02-02 20:57:33 +0630

    ToDoList.sql: Added 'updated_at' in 'users' table, 'completed_at' in 'parent_tasks', 'child_tasks', and alter column 'is_completed' to 'completed' in 'parent_tasks', 'child_tasks'

--- src/main/java/dev/jade/todolist/db/ToDoList.sql
+++ src/main/java/dev/jade/todolist/db/ToDoList.sql
@@ -2,9 +2,9 @@ DO
 $$
     BEGIN
         CREATE TYPE priorities AS ENUM (
-            'goblin', -- low
-            'minotaur', -- medium
-            'dragon' -- high
+            'low',
+            'medium',
+            'high'
             );
     EXCEPTION
         WHEN duplicate_object THEN NULL;
@@ -17,40 +17,43 @@ CREATE TABLE IF NOT EXISTS users
     username   VARCHAR(50)  NOT NULL,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(128) NOT NULL,
-    created_at TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS sections
 (
-    section_id   BIGSERIAL PRIMARY KEY,
-    section_name VARCHAR(50) NOT NULL,
-    created_at   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id      BIGINT      NOT NULL REFERENCES users (user_id)
+    section_id BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id    BIGINT      NOT NULL REFERENCES users (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS parent_tasks
 (
-    parent_id    BIGSERIAL PRIMARY KEY,
-    description  VARCHAR(255) NOT NULL,
-    deadline     TIMESTAMPTZ           DEFAULT NULL,
-    priority     priorities   NOT NULL DEFAULT 'goblin',
-    is_completed BOOLEAN      NOT NULL DEFAULT FALSE,
-    created_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    section_id   BIGINT       NOT NULL REFERENCES sections (section_id)
+    parent_task_id BIGSERIAL PRIMARY KEY,
+    title          VARCHAR(255) NOT NULL,
+    deadline       TIMESTAMP             DEFAULT NULL,
+    priority       priorities   NOT NULL DEFAULT 'low',
+    completed      BOOLEAN      NOT NULL DEFAULT FALSE,
+    completed_at   TIMESTAMP             DEFAULT NULL,
+    created_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    section_id     BIGINT       NOT NULL REFERENCES sections (section_id)
 );
 
 CREATE TABLE IF NOT EXISTS child_tasks
 (
-    child_id     BIGSERIAL PRIMARY KEY,
-    description  VARCHAR(255) NOT NULL,
-    deadline     TIMESTAMPTZ           DEFAULT NULL,
-    priority     priorities   NOT NULL DEFAULT 'goblin',
-    is_completed BOOLEAN      NOT NULL DEFAULT FALSE,
-    created_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    parent_id    BIGINT       NOT NULL REFERENCES parent_tasks (parent_id)
+    child_task_id  BIGSERIAL PRIMARY KEY,
+    title          VARCHAR(255) NOT NULL,
+    deadline       TIMESTAMP             DEFAULT NULL,
+    priority       priorities   NOT NULL DEFAULT 'low',
+    completed      BOOLEAN      NOT NULL DEFAULT FALSE,
+    completed_at   TIMESTAMP             DEFAULT NULL,
+    created_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    parent_task_id BIGINT       NOT NULL REFERENCES parent_tasks (parent_task_id)
 );
 
 CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS TRIGGER AS
@@ -76,15 +79,13 @@ $$
             FOR EACH ROW
         EXECUTE FUNCTION update_updated_at_column();
 
-        DROP TRIGGER IF EXISTS update_parent_tasks_updated_at ON parent_tasks;
-        CREATE TRIGGER update_parent_tasks_updated_at
+        CREATE OR REPLACE TRIGGER update_parent_tasks_updated_at
             BEFORE UPDATE
             ON parent_tasks
             FOR EACH ROW
         EXECUTE FUNCTION update_updated_at_column();
 
-        DROP TRIGGER IF EXISTS update_child_tasks_updated_at ON child_tasks;
-        CREATE TRIGGER update_child_tasks_updated_at
+        CREATE OR REPLACE TRIGGER update_child_tasks_updated_at
             BEFORE UPDATE
             ON child_tasks
             FOR EACH ROW
